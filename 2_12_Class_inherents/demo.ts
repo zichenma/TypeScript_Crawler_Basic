a59f9a3dca15f5929878dfe061483e30947fff48
zichen ma <zma@fundopolis.com>
2020-01-30 06:27:26 -0500

    Finished 2_12

---
+++ 2_12_Class_inherents/demo.ts
@@ -0,0 +1,33 @@
+class Person {
+  name = 'dell'; // 定义类属性
+  getName() {
+    return this.name;
+  }
+}
+
+const person = new Person();
+
+// 类继承
+
+class Teacher extends Person {
+  getTeacherName() {
+    return 'techer';
+  }
+}
+// 多太：
+class Teacher1 extends Person {
+  getName() {
+    return 'lee';
+  }
+  getParentName() {
+    // super 可以指向父类中的方法
+    return super.getName() + ' lee';
+  }
+}
+// 运行 TS： ts-node demo.ts
+const teacher = new Teacher();
+console.log(teacher.getName()); // dell
+console.log(teacher.getTeacherName()); // teacher
+const teacher1 = new Teacher1();
+console.log(teacher1.getName()); // lee
+console.log(teacher1.getParentName()); // dell lee
